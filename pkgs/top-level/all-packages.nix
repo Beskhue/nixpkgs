dd122a4e428a6a99b5f2ce626620c7b456ab6c92
MetaDark <kira.bruneau@gmail.com>
2020-06-26 22:24:35 -0400

    zynaddsubfx: use zyn-fusion as default gui module
    - Added `guiModule` option that accepts "fltk", "ntk", "zest", or "off"
    
    - Split FLTK dependencies from NTK dependencies
    
    - Added support for building the FLTK gui
    
    - Added support for building the Zyn-Fusion (zest) gui
    
    - Added new derivation for the Zest UI framework (local to zynaddsubfx)
      It's not yet designed to be used outside of zynaddsubfx, but it
      may be in the future
    
    - Added flags for all optional features
    
    - Added & disabled `dssiSupport` by default
    
    - Disabled `lashSupport` by default
      Slows down startup looking for LASH server if not running
    
    - Added & enabled `portaudioSupport` by default
      Cross platform audio library that uses ALSA/JACK on Linux.
      Supports multiple audio streams without needing JACK.
    
    - Enabled tests

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24738,7 +24738,19 @@ in
 
   zscroll = callPackage ../applications/misc/zscroll {};
 
-  zynaddsubfx = callPackage ../applications/audio/zynaddsubfx { };
+  zynaddsubfx = zyn-fusion;
+
+  zynaddsubfx-fltk = callPackage ../applications/audio/zynaddsubfx {
+    guiModule = "fltk";
+  };
+
+  zynaddsubfx-ntk = callPackage ../applications/audio/zynaddsubfx {
+    guiModule = "ntk";
+  };
+
+  zyn-fusion = callPackage ../applications/audio/zynaddsubfx {
+    guiModule = "zest";
+  };
 
   ### BLOCKCHAINS / CRYPTOCURRENCIES / WALLETS
 
