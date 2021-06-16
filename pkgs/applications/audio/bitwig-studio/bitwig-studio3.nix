37546b19aaa54d03db4ce4dc63ac0086c1f2bf7b
Thomas Churchman <thomas@kepow.org>
2021-06-16 09:08:28 +0100

    bitwig-studio: suffix LD_LIBRARY_PATH
    NixOS's `services.pipewire.jack` and `pw-jack` set `LD_LIBRARY_PATH`, we
    need to ensure those binaries are prioritized

--- pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
+++ pkgs/applications/audio/bitwig-studio/bitwig-studio3.nix
@@ -34,6 +34,8 @@ stdenv.mkDerivation rec {
   ldLibraryPath = lib.strings.makeLibraryPath buildInputs;
 
   installPhase = ''
+    runHook preInstall
+
     mkdir -p $out/bin
     cp -r opt/bitwig-studio $out/libexec
     ln -s $out/libexec/bitwig-studio $out/bin/bitwig-studio
@@ -41,6 +43,8 @@ stdenv.mkDerivation rec {
     substitute usr/share/applications/bitwig-studio.desktop \
       $out/share/applications/bitwig-studio.desktop \
       --replace /usr/bin/bitwig-studio $out/bin/bitwig-studio
+
+      runHook postInstall
   '';
 
   postFixup = ''
